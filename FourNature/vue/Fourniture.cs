69bf03c81ae445a7716cc4258fe7524ee9ff5d6a
Axel <axl5495@gmail.com>
2017-07-10 18:45:01 +0200

    achitecture

---
+++ FourNature/vue/Fourniture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FourNature.vue
+{
+    public partial class Fourniture : Form
+    {
+        public Fourniture()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
